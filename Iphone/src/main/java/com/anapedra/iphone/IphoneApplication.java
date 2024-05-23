8ca4a1355dbe5cdd177bc77c4860fa7ff6906918
anapedra <anapedra.mil@gmail.com>
2024-05-22 22:53:00 -0300

    Prject iPhone created

---
+++ Iphone/src/main/java/com/anapedra/iphone/IphoneApplication.java
@@ -0,0 +1,13 @@
+package com.anapedra.iphone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IphoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IphoneApplication.class, args);
+    }
+
+}
